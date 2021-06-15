10c98a710939a5687d0ed490617e1a2d124eafdc
Jesus Recio <jrecio@izertis.com>
2021-06-15 14:29:16 +0200

    increase wait for fab

--- src/main/java/android/FileListPage.java
+++ src/main/java/android/FileListPage.java
@@ -101,14 +101,14 @@ public class FileListPage extends CommonPage {
 
     public void createFolder() {
         Log.log(Level.FINE, "Starts: create folder");
-        waitById(5, fabButton);
+        waitById(15, fabButton);
         fabButton.click();
         createFolder.click();
     }
 
     public void upload() {
         Log.log(Level.FINE, "Starts: upload");
-        waitById(5, fabButton);
+        waitById(15, fabButton);
         fabButton.click();
         uploadOption.click();
     }
