25ca85ddfde2771d6eb15397cd2016f7754afe23
Jesus Recio <jrecio@izertis.com>
2025-08-19 09:05:05 +0200

    fix: set a compatible version of uiautomator driver with the current appium sopported version

--- runAppium.sh
+++ runAppium.sh
@@ -2,5 +2,5 @@
 set -ex
 npm install -g appium@2.19.0
 appium -v
-appium driver install uiautomator2
+appium driver install uiautomator2@4.2.9
 appium --allow-insecure=adb_shell --allow-cors --log-level debug &>/dev/null &
