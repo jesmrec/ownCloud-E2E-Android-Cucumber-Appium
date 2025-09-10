f43d429b0c86f38ed697db569d817afd7c4ae4d6
Jesus Recio <jrecio@izertis.com>
2025-09-10 16:58:43 +0200

    feat: update appium and uiautomator driver to latest

--- runAppium.sh
+++ runAppium.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-npm install -g appium@2.19.0
+npm install -g appium@latest
 appium -v
-appium driver install uiautomator2@4.2.9
-appium --allow-insecure=adb_shell --allow-cors --log-level debug &>/dev/null &
+appium driver install uiautomator2@latest
+appium --allow-insecure='*:adb_shell' --allow-cors --log-level debug &>/dev/null &
